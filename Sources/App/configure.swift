13b8a8d1c00fc9e7b5da37db32642527f9cd0761
Zach 'Leif' Eriksen <zeriksen@clientresourcesinc.com>
2019-02-28 21:08:26 -0600

    API-1: Created the basic Auth seq

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,32 +1,39 @@
 import FluentSQLite
 import Vapor
+import Authentication
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    let serverConfiure = NIOServerConfig.default(hostname: "localhost", port: 9090)
+    services.register(serverConfiure)
     // Register providers first
     try services.register(FluentSQLiteProvider())
-
+    
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     // Register middleware
+    try services.register(AuthenticationProvider())
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(SessionsMiddleware.self)
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
+    
+    config.prefer(MemoryKeyedCache.self, for: KeyedCache.self)
+    
     // Configure a SQLite database
     let sqlite = try SQLiteDatabase(storage: .memory)
-
+    
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     databases.add(database: sqlite, as: .sqlite)
     services.register(databases)
-
+    
     // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: User.self, database: .sqlite)
     services.register(migrations)
 }
